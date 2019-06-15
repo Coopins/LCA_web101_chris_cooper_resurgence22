ea5d889283dfaf6601f13685ce810add84bfaba6
Coopins <coopertime08@gmail.com>
2019-06-15 16:49:05 -0500

    stressReliever2.html

---
+++ MyPortfolio/Stress Reliever (CP3)/stressReliever.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //  Place everything you want to build between here...
+    
+
+        
+    // this holds the value of the number of items in Our List
+    
+    
+    
+    
+    // ...and here.
+    }
